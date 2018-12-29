80a318b0eb4c2faf46fbe395ded45521d5b13056
António Alves <runeterraf@gmail.com>
2018-12-29 20:08:46 +0000

    Leitura de ficheiros txt

--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -3,11 +3,21 @@ package Demo;
 
 import JFrames.PesquisaJFrame;
 import Core.Pesquisa;
+import LeituraFicheiros.LeituraFicheiros;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /**
         PesquisaJFrame pf = new PesquisaJFrame(new Pesquisa());
-        pf.setVisible(true);
+        pf.setVisible(true);**/
+        LeituraFicheiros file = new LeituraFicheiros("files/");
+        String[] lista = file.lerDocumentos();
+        for(int i=0; i<lista.length; i++){
+            System.out.println(lista[i]);
+        }
     }
 
 }
--- src/main/java/LeituraFicheiros/LeituraFicheiros.java
+++ src/main/java/LeituraFicheiros/LeituraFicheiros.java
@@ -1,12 +1,53 @@
 package LeituraFicheiros;
 
-public class LeituraFicheiros {
-    int numDocs;
 
-    public LeituraFicheiros() {
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LeituraFicheiros {
+    int numDocs ;
+    String path ;
+
+    public LeituraFicheiros(String path) {
+        this.path = path;
+        File folder = new File(this.path);
+        File[] list = folder.listFiles();
+        numDocs = list.length;
     }
 
     public int getNumDocs() {
         return numDocs;
+    }
+
+    public String[] lerDocumentos() throws IOException {
+        //Lista de ficheiros
+        File folder = new File(this.path);
+        File[] list = folder.listFiles();
+
+        //Lista de strings com os conteudos dos ficheiros
+        String[] lista = new String[this.numDocs];
+
+        for(int i=0; i<list.length; i++){
+            String name = list[i].getName();
+            System.out.println(name);
+            InputStream is = new FileInputStream(path + name);
+            BufferedReader buf = new BufferedReader((new InputStreamReader(is)));
+
+            String line = buf.readLine();
+            StringBuilder sb = new StringBuilder();
+
+            while(line != null){
+                sb.append(line);
+                line = buf.readLine();
+            }
+
+            lista[i] = sb.toString();
+            System.out.println("Contents: " + lista[i]);
+        }
+
+
+        return lista;
     }
 }
